1781b1e8639fc0a97fbe96f77c72cc4fb7ef309f
Thomas Balzer <thomas.balzer16@gmail.com>
2024-05-01 18:57:58 -0600

    debug

--- PlaySong.py
+++ PlaySong.py
@@ -18,18 +18,22 @@ def play_audio(file_path):
     def callback(outdata, frames, time, status):
         try:
             data = next(data_generator)
+            if len(data) < outdata.shape[0]:
+                # If the last block is shorter than the block size, pad with zeros
+                data = np.pad(data, (0, outdata.shape[0] - len(data)), mode='constant')
             outdata[:] = data
         except StopIteration:
             outdata.fill(0)  # Fill remaining buffer with zeros
             raise sd.CallbackStop
 
-    # Get the sample rate from the file
+    # Get the sample rate and channel count from the file
     with sf.SoundFile(file_path) as sf_file:
         fs = sf_file.samplerate
+        channels = sf_file.channels
 
     # Setting up the audio stream for playback
-    with sd.OutputStream(samplerate=fs, channels=sf_file.channels, callback=callback, blocksize=block_size):
-        sd.wait()  # Wait until file is done playing
+    with sd.OutputStream(samplerate=fs, channels=channels, callback=callback, blocksize=block_size):
+        input("Press Enter to stop playback...")  # Keeps the stream active until Enter is pressed
 
 def main():
     directory = 'songs'  # Directory containing the songs
